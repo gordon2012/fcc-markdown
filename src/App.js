7d01df2b88d6f6f1403df48c37c610150f33b29f
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-07-02 23:18:26 -0700

    markdown WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import marked from 'marked';
+window.marked = marked;
 
 class App extends Component {
+  state = {
+    input: [
+      `# A header
+## A subheader
+[Google](https://google.com)`,
+
+      'The `const` keyword is great.',
+
+      '\n```js' +
+        'for(int i = 0; i++ i < 10) {' +
+        'console.log(i);' +
+        '}' +
+        '```',
+
+      `- one
+- two
+- three
+
+> block
+> this.state = {loading = true};
+> quote
+
+![Dank](http://gordonscampinggear.com/img/portfolio.png)
+
+**BOLD!!**`
+    ].join('')
+  };
+
+  getMarkdown(input) {
+    return { __html: marked(input, { sanitize: true }) };
+  }
+
+  setInput = e => {
+    this.setState({ input: e.target.value });
+  };
+
   render() {
+    const { input } = this.state;
     return (
       <div className="App">
-        <div />
+        <textarea
+          name=""
+          id="editor"
+          cols="30"
+          rows="10"
+          onChange={this.setInput}
+          value={this.state.input}
+        />
+
+        <div id="preview" dangerouslySetInnerHTML={this.getMarkdown(input)} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import marked from 'marked';
+
+// require('expose-loader?marked!marked');
+import exposeLoader from 'expose-loader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
